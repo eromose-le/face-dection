1d34a512a7be4f72be9fb3d0190747f7460daf75
eromose-le <erons24@yahoo.com>
2021-12-11 16:53:17 +0100

    draw and position canvas from img

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,30 @@ function App() {
   const imgRef = useRef();
   const canvasRef = useRef();
 
+  const handleImage = async () => {
+    // Pass img to model
+    const detections = await faceapi
+      .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(imgRef.current);
+    // Position canvas
+    faceapi.matchDimensions(canvasRef.current, {
+      width: 940,
+      height: 650
+    });
+
+    // Resize canvas
+    const resized = faceapi.resizeResults(detections, {
+      width: 940,
+      height: 650
+    });
+
+    // Position canvas from Img
+    faceapi.draw.drawDetections(canvasRef.current, resized);
+  };
+
   useEffect(() => {
     const loadModels = () => {
       Promise.all([
@@ -13,7 +37,7 @@ function App() {
         faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
         faceapi.nets.faceExpressionNet.loadFromUri('/models')
       ])
-        .then(console.log("it's loaded"))
+        .then(handleImage)
         .catch((e) => console.log(e));
     };
 
@@ -22,6 +46,7 @@ function App() {
   return (
     <div className="app">
       <img
+        crossOrigin="anonymous"
         ref={imgRef}
         src="https://images.pexels.com/photos/1537635/pexels-photo-1537635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
